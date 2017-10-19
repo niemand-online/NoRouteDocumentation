c24fe4037d7747f2b520b55515653c01b354bdd1
Joshua Behrens <behrens@ksk-agentur.de>
2017-10-20 01:00:20 +0200

    Initialised plugin

---
+++ NoRouteDocumentation.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace NoRouteDocumentation;
+
+use Shopware\Framework\Plugin\Plugin;
+
+/**
+ * Class NoRouteDocumentation
+ * @package NoRouteDocumentation
+ */
+class NoRouteDocumentation extends Plugin
+{
+}
